2d149f31be531d758aa5533c18bb3262c88f7c49
kai <luanvuforwork@gmail.com>
2025-05-10 02:01:56 +0700

    refactor: update ColourfulText effect dependencies and replace anchor with Link in NavbarLogo

--- components/ui/resizable-navbar.tsx
+++ components/ui/resizable-navbar.tsx
@@ -7,6 +7,7 @@ import {
   useScroll,
   useMotionValueEvent,
 } from "motion/react";
+import Link from "next/link";
 
 import React, { useRef, useState } from "react";
 
@@ -233,7 +234,7 @@ export const MobileNavToggle = ({
 
 export const NavbarLogo = () => {
   return (
-    <a
+    <Link
       href="/"
       className="relative z-20 mr-4 flex items-center space-x-2 px-2 py-1 text-sm font-normal text-black"
     >
@@ -245,7 +246,7 @@ export const NavbarLogo = () => {
         height={50}
       />
       <span className="font-medium text-xl  text-black dark:text-white">kai.</span>
-    </a>
+    </Link>
   );
 };
 
